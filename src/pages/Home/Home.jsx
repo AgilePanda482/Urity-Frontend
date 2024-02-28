03b6dfa495ef3f51c55d7113325a2785efc2be37
David Vela <dsrv06@gmail.com>
2024-02-27 21:30:03 -0600

    test: websockets react

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,26 @@
+import { data } from "autoprefixer"
 import NavbarComponent from "../../components/Navbar/Navbar"
+import { io } from 'socket.io-client'
+
+const socket = io('/')
+let infoStudent;
 
 function Home() {
+
+  socket.on('UID', (data) => {
+    infoStudent = data;
+    console.log(data)
+  })
+
+
   const theObject = {
-    name: 'Javier',
-    age: 23,
-    country: 'Colombia',
-    city: 'Medellin',
+    codigo: infoStudent.codigo,
+    UID: infoStudent.UID,
+    Nombre: infoStudent.Nombre,
+    DatoAcademico: infoStudent.DatoAcademico,
 
     getFullName: function () {
-      return `${this.name} ${this.age} ${this.country} ${this.city}`
+      return `${this.codigo} ${this.UID} ${this.Nombre} ${this.DatoAcademico}`
     }
   }
 
@@ -18,10 +30,10 @@ function Home() {
       <div className="h-screen text-white w-1/2 m-auto bg-slate-500 rounded-2xl overflow-hidden">
         <div className="h-screen flex-col gap-5 bg-slate-500 flex justify-center items-center text-center">
           <h1 className="text-5xl font-bold">Home</h1>
-          <p className="text-xl">{theObject.name}</p>
-          <p className="text-xl">{theObject.age}</p>
-          <p className="text-xl">{theObject.country}</p>
-          <p className="text-xl">{theObject.city}</p>
+          <p className="text-xl">{theObject.codigo}</p>
+          <p className="text-xl">{theObject.UID}</p>
+          <p className="text-xl">{theObject.Nombre}</p>
+          <p className="text-xl">{theObject.DatoAcademico}</p>
           <p className="text-xl text-red-500">{theObject.getFullName()}</p>
         </div>
       </div>
