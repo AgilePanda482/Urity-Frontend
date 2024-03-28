f666021d389166bd6884606a7bd0b3d2a0f0e47a
David Vela <dsrv06@gmail.com>
2024-03-27 20:32:18 -0600

    fixed login problems

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const API = "http://localhost:4000/api/auth/";
 
-export const loginRequest = (user) => axios.post(`${API}login`, user);
+export const loginRequest = (user) => axios.post(`${API}/login`, user);
 
 ///api/auth/login
