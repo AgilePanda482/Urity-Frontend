898ce127514bcaf9fb6e8cb2908bc1f5cc862f96
rincondeveloper <4braham3mma@gmail.com>
2024-04-01 12:49:29 -0600

    Fixing details at ProtectedRoute/Notfound

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Spinner } from "@nextui-org/react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/authContext";
 
@@ -7,7 +9,7 @@ function ProtectedRoute() {
   if (loading)
     return (
       <div className="flex justify-center items-center text-white bg-black h-screen text-md">
-        Loading...
+        <Spinner />
       </div>
     );
   if (!loading && !isLogin) return <Navigate to="/login" replace />;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,11 +5,11 @@ function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full bg-black">
       <div>
-        <h1 className="text-white text-5xl font-bold text-center">
+        <h1 className="text-white text-4xl font-bold text-center">
           Error 404 - NotFound
         </h1>
       </div>
-        <img className="absolute bottom-0 right-0 w-1/2 sm:w-44 md:w-2/4 lg:w-1/3 xl:w-1/4" src={NotFoundImage} alt="Cat not found" />
+        <img className="absolute bottom-0 right-0 w-1/3 sm:w-40 md:w-1/4 lg:w-1/4 xl:w-1/4" src={NotFoundImage} alt="Cat not found" />
     </div>
   );
 }
